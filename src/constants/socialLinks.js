8039afdc9d9f9e0215d424b2303fc70107ee4191
oliverwalsh7 <oliverwalsh7@gmail.com>
2020-12-30 00:47:20 -0500

    projects page

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -9,18 +9,18 @@ const data = [
   {
     id: 1,
     icon: <FaGithubSquare className="social-icon"></FaGithubSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.github.com/oliverwalsh7",
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
+    url: "https://www.linkedin.com/in/vasilios-walsh",
   },
   {
     id: 3,
     icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
-    url: "https://www.twitter.com",
-  }
+    url: "https://www.twitter.com/vasilios47",
+  },
 ]
 const links = data.map(link => {
   return (
